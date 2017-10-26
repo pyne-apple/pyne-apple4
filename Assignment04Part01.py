0a8bcbea450c1004ad83aeaa0dda47f10ee4b092
John Bales <johntbalesg@gmail.com>
2017-10-26 16:55:47 -0500

    Attempt with pandas

--- Assignment04Part01.py
+++ Assignment04Part01.py
@@ -34,7 +34,6 @@ def main():
         # print(trainingDF)
 
         # Weights
-        
         weights = [[0] * (len(headers)-1)] # Weight for each attribute
         weightHeaders = []
         for header in headers:
@@ -47,14 +46,26 @@ def main():
         # print(weightsDF)
 
         # For each iteration 
-        # for i in range(0, iterations, 1):
+        for i in range(0, iterations, 1):
+            # output = sigmoid(pd.DataFrame.dot(trainingDF.iloc[i][0:-1], weightsDF.iloc[0]))
+            # weightsDF.iloc[0] += pd.DataFrame.dot(trainingDF.iloc[i][0:-1], (trainingDF.iloc[i][-1] - output) * output * (1 - output))
+
             # For each weight
 
+
+# for iteration in xrange(10000):
+#     output = 1 / (1 + exp(-(dot(training_set_inputs, synaptic_weights))))
+#     synaptic_weights += dot(training_set_inputs.T, (training_set_outputs - output) * output * (1 - output))
+# print 1 / (1 + exp(-(dot(array([1, 0, 0]), synaptic_weights))))
 
             
             # Print iteration, weights, output
             # After iteration 1: w(A1)=0.324, w(A2)=-0.021, w(A3)=3.414, output=2.353
+            weightsPrint = ""
+            for weight in weightsDF.iloc[0]:
+                weightsPrint += weight;
 
+            print("After iteration " + str(i) + ": " + weightsPrint + "output=" + str(output))
 
 
     # All the network weights must be initialized to 0.
@@ -64,10 +75,16 @@ def main():
     
     # Reading in data from test file
     # with open(testFileName) as testFile:
-        
 
+# def evaluate(self, test_data):
+#     test_results = [(np.argmax(self.feedforward(x)), y) for (x, y) in test_data]
+#     return sum(int(x == y) for (x, y) in test_results)
 
+# def sigmoid(z):
+#     return 1.0/(1.0+np.exp(-z))        
 
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
 
 if __name__ == "__main__":
     main()
