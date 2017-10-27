ad5ba5efcb825d8a5ca9a1588e3261f93453035d
John Bales <johntbalesg@gmail.com>
2017-10-26 19:00:36 -0500

    trying formula, messing up hardcore

--- Assignment04Part01.py
+++ Assignment04Part01.py
@@ -24,17 +24,23 @@ def main():
         headers = firstLine.split()
         # print(headers)
 
+        classLists = []
         trainingLists = []
         for line in trainingFile:
-            trainingLists.append(list(int(x) for x in line.strip().replace('\t', '')))
+            splitValues = list(int(x) for x in line.strip().replace('\t', ''))
+            classLists.append([splitValues[-1]])
+            splitValues.pop(-1) # Remove class
+            trainingLists.append(splitValues)
+        # print(classLists)
         # print(trainingLists)
 
-        # Convert training data into a pandas dataframe
-        trainingDF = pd.DataFrame.from_records(trainingLists, columns=headers)
-        # print(trainingDF)
+        x = np.array(trainingLists)
+        y = np.array(classLists)
+        # print(x)
+        # print(y)
 
         # Weights
-        weights = [[0] * (len(headers)-1)] # Weight for each attribute
+        weights = [[0.0] * (len(headers)-1)] # All the network weights must be initialized to 0.
         weightHeaders = []
         for header in headers:
             if header == "class":
@@ -42,33 +48,51 @@ def main():
             weightHeaders.append("w(" + header + ")")
         # print(weights)
         # print(weightHeaders)
-        weightsDF = pd.DataFrame.from_records(weights, columns=weightHeaders)
-        # print(weightsDF)
+        weightsNPA = np.array(weights)
+        # print(weightsNPA)
+        
+        # For each iteration 
+        for i in range(iterations):
+            # print(x[i])
+            # print(float(y[i]))
+            # sig = sigmoid(np.dot(weightsNPA, x[i]))
+            crossProduct = np.dot(weightsNPA, x[i])
+            error = float(y[i]) - sigmoid(crossProduct)
+            # print(sigmoid(crossProduct))
+            print(error)
+            
+            weightsNPA += learningRate * error * weightsNPA * sigmoidPrime(crossProduct)
+            print(weightsNPA)
+
+            
+            
+
+            # H = sigmoid(np.dot(X, Wh))                  # hidden layer results
+            # Z = np.dot(H,Wz)                            # output layer, no activation
+            # E = Y - Z                                   # how much we missed (error)
+            # dZ = E * L                                  # delta Z
+            # Wz +=  H.T.dot(dZ)                          # update output layer weights
+            # dH = dZ.dot(Wz.T) * sigmoid_(H)             # delta H
+            # Wh +=  X.T.dot(dH)  
+
 
         # For each iteration 
-        for i in range(0, iterations, 1):
+        # for i in range(0, iterations, 1):
             # output = sigmoid(pd.DataFrame.dot(trainingDF.iloc[i][0:-1], weightsDF.iloc[0]))
             # weightsDF.iloc[0] += pd.DataFrame.dot(trainingDF.iloc[i][0:-1], (trainingDF.iloc[i][-1] - output) * output * (1 - output))
 
             # For each weight
-
-
-# for iteration in xrange(10000):
-#     output = 1 / (1 + exp(-(dot(training_set_inputs, synaptic_weights))))
-#     synaptic_weights += dot(training_set_inputs.T, (training_set_outputs - output) * output * (1 - output))
-# print 1 / (1 + exp(-(dot(array([1, 0, 0]), synaptic_weights))))
-
             
             # Print iteration, weights, output
             # After iteration 1: w(A1)=0.324, w(A2)=-0.021, w(A3)=3.414, output=2.353
-            weightsPrint = ""
-            for weight in weightsDF.iloc[0]:
-                weightsPrint += weight;
+            # weightsPrint = ""
+            # for weight in weightsDF.iloc[0]:
+            #     weightsPrint += weight;
 
-            print("After iteration " + str(i) + ": " + weightsPrint + "output=" + str(output))
+            # print("After iteration " + str(i) + ": " + weightsPrint + "output=" + str(output))
 
 
-    # All the network weights must be initialized to 0.
+    
 
 
     # when printing the weight values, print exactly the four digits after the decimal place.
@@ -76,15 +100,17 @@ def main():
     # Reading in data from test file
     # with open(testFileName) as testFile:
 
-# def evaluate(self, test_data):
-#     test_results = [(np.argmax(self.feedforward(x)), y) for (x, y) in test_data]
-#     return sum(int(x == y) for (x, y) in test_results)
 
-# def sigmoid(z):
-#     return 1.0/(1.0+np.exp(-z))        
+# activation function
+def sigmoid(x): 
+    return 1/(1 + np.exp(-x))      
+    
+# derivative of sigmoid
+def sigmoidPrime(x): 
+    return x * (1 - x)             
 
-def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
+# def sigmoid(x):
+#     return 1 / (1 + math.exp(-x))
 
 if __name__ == "__main__":
     main()
